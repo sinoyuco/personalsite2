4cc8a0f4a9947384115e4b1e86a310fbde4ec838
Sinan Yucesan <sinan.yucesan@gmail.com>
2021-01-28 13:23:23 -0500

    info general

--- psite/src/component/info/general.jsx
+++ psite/src/component/info/general.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 const General = () => {
     return (
-        <div>
-            <p>General</p>
+        <div className="general-main">
+
+            <div className="general-pp-container"><img className="general-pp" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/IMG_9781.JPG" /></div>
+            
+            <h2>Hello, my name is Sinan.</h2>
+
+            <p> I am a software engineer with a passion for developing technical tools and applications to solve everyday problems and improve everyday processes. I cherish the opportunity to interact with new frameworks and libraries.</p>
+
+            <p>Through my time at App Academy, I've had the opportunity to build full stack applications with everyday uses utilizing mainly Javascript and Ruby along with other technologies and frameworks. I aspire to continue this journey and grow as a software engineer, learning new technologies and constructing new solutions along the way.</p>
         </div>
     )
 };
 
 export default General;
+
+

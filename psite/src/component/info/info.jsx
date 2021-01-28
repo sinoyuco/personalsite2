b3b5562112e86e251841efefd46d3d4d363b50d2
Sinan Yucesan <sinan.yucesan@gmail.com>
2021-01-28 16:23:29 -0500

    info skills

--- psite/src/component/info/info.jsx
+++ psite/src/component/info/info.jsx
@@ -15,15 +15,15 @@ const Info = () => {
         <div className="info-main">
             <div className="info-bar">
                 <div onClick={() => setSection('General')} className="info-general-button">
-                    <i class="fas fa-info-circle"></i>
+                    <i className="fas fa-info-circle"></i>
                 </div>
                 <div className="info-bar-separator"></div>
                 <div onClick={() => setSection('Skills')} className="info-skills-button">
-                    <i class="fas fa-code"></i>
+                    <i className="fas fa-code"></i>
                 </div>
                 <div className="info-bar-separator"></div>
                 <div className="info-resume-button">
-                    <i class="fas fa-download"></i>
+                    <i className="fas fa-download"></i>
                 </div>
 
             </div>
--- psite/src/component/info/skills.jsx
+++ psite/src/component/info/skills.jsx
@@ -2,8 +2,42 @@ import React from 'react';
 
 const Skills = () => {
     return (
-        <div>
-            <p>SKILLS</p>
+        <div className="skills-main">
+            <div className="skills-row">
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/js.png"/></div>
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/html.png" /></div>
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/css.png" /></div>
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/react.png" /></div>
+            </div>
+
+            <div className="skills-row">
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/redux.png" /></div>
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/ruby.png" /></div>
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/rails.png" /></div>
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/python.png" /></div>
+            </div>
+
+            <div className="skills-row">
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/node.png" /></div>
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/mongo.png" /></div>
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/express.png" /></div>
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/jquery.png" /></div>
+            </div>
+
+            <div className="skills-row">
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/postgre.png" /></div>
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/git.png" /></div>
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/webpack.png" /></div>
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/docker.png" /></div>
+            </div>
+
+            <div className="skills-row">
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/s3.png" /></div>
+                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/powerbi.png" /></div>
+                <div className="skills-child"></div>
+                <div className="skills-child"></div>
+            </div>
+            
         </div>
     )
 };

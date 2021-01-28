aa9e26664e74dc4bafd6c011364d4b713aa91d66
Sinan Yucesan <sinan.yucesan@gmail.com>
2021-01-28 17:15:36 -0500

    info skills styling

--- psite/src/component/info/skills.jsx
+++ psite/src/component/info/skills.jsx
@@ -4,36 +4,36 @@ const Skills = () => {
     return (
         <div className="skills-main">
             <div className="skills-row">
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/js.png"/></div>
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/html.png" /></div>
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/css.png" /></div>
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/react.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>JavaScript</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/js.png"/></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>HTML5</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/html.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>CSS3</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/css.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>React</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/react.png" /></div>
             </div>
 
             <div className="skills-row">
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/redux.png" /></div>
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/ruby.png" /></div>
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/rails.png" /></div>
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/python.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>Redux</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/redux.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>Ruby</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/ruby.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>Ruby on Rails</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/rails.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>Python3</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/python.png" /></div>
             </div>
 
             <div className="skills-row">
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/node.png" /></div>
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/mongo.png" /></div>
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/express.png" /></div>
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/jquery.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>Node.js</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/node.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>MongoDB</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/mongo.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>Express.js</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/express.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>jQuery</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/jquery.png" /></div>
             </div>
 
             <div className="skills-row">
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/postgre.png" /></div>
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/git.png" /></div>
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/webpack.png" /></div>
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/docker.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>PostgreSQL</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/postgre.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>Git</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/git.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>Webpack</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/webpack.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>Docker</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/docker.png" /></div>
             </div>
 
             <div className="skills-row">
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/s3.png" /></div>
-                <div className="skills-child"><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/powerbi.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>AWS S3</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/s3.png" /></div>
+                <div className="skills-child"><div className="skills-child-dropdown"><p>Power BI</p></div><img className="skills-child-image" src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/icons/powerbi.png" /></div>
                 <div className="skills-child"></div>
                 <div className="skills-child"></div>
             </div>

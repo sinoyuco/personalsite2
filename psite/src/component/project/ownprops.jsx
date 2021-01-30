769e93893d57c58eccb32ecae032edf4e0fd11f8
Sinan Yucesan <sinan.yucesan@gmail.com>
2021-01-29 19:56:23 -0500

    ownprops section skeleton

--- psite/src/component/project/ownprops.jsx
+++ psite/src/component/project/ownprops.jsx
@@ -2,8 +2,28 @@ import React from 'react';
 
 const OwnProps = () => {
     return(
-        <div>
-            <p>OWN PROPS</p>
+        <div className="project-main">
+
+            <div className="project-gif">
+                <a className="project-gif-link" href="https://aa-ownpropsdotmatch.herokuapp.com/" target="_blank">
+                    <img src="https://hiptrip-aa-seed.s3.amazonaws.com/psite/OPM.gif" alt="" />
+                </a>
+            </div>
+
+
+            <div className="project-dropdown">
+                <div className="project-header">
+                    <h2>OWNPROPS.MATCH</h2>
+                </div>
+
+                <div className="project-links">
+
+                    <a href="https://aa-ownpropsdotmatch.herokuapp.com/" target="_blank"><h2>Live</h2></a>
+                    <div className="project-links-separator"></div>
+                    <a href="https://github.com/sinoyuco/OwnProps.Match" target="_blank"><h2>Github</h2></a>
+
+                </div>
+            </div>
         </div>
     )
 };
